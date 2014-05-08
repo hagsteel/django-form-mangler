917f3aee69d2cdb1319cec308e414eb2713bc5fa
Jonas Hagstedt <hagstedt@gmail.com>
2014-05-09 00:22:47 +0200

    Added boostrap3 tag

--- form_mangler/templatetags/mangler.py
+++ form_mangler/templatetags/mangler.py
@@ -6,33 +6,38 @@ from django.template import Context
 register = template.Library()
 
 
-def get_widget_template_name(field):
+def get_widget_template_name(field, use_bootstrap=False):
+    widget_dir = 'widgets'
+    if use_bootstrap:
+        widget_dir = 'widgets/bootstrap3'
+
     if isinstance(field.widget, widgets.DateInput):
-        return 'widgets/date_input_field.html'
+        return '{}/date_input_field.html'.format(widget_dir)
     if isinstance(field.widget, widgets.TextInput):
-        return 'widgets/text_input_field.html'
+        return '{}/text_input_field.html'.format(widget_dir)
     if isinstance(field.widget, widgets.Textarea):
-        return 'widgets/textarea_field.html'
+        return '{}/textarea_field.html'.format(widget_dir)
     elif isinstance(field.widget, widgets.CheckboxSelectMultiple):
-        return 'widgets/checkbox_select_multiple.html'
+        return '{}/checkbox_select_multiple.html'.format(widget_dir)
     elif isinstance(field.widget, widgets.Select):
-        return 'widgets/select.html'
+        return '{}/select.html'.format(widget_dir)
     elif isinstance(field.widget, widgets.CheckboxInput):
-        return 'widgets/checkbox.html'
+        return '{}/checkbox.html'.format(widget_dir)
     else:
-        return 'widgets/default.html'
+        return '{}/default.html'.format(widget_dir)
 
 
 class MangleWidgetNode(template.Node):
-    def __init__(self, field_name, extra_attributes):
+    def __init__(self, field_name, extra_attributes, use_bootstrap):
         self.extra_attributes = extra_attributes
         self.field_name = field_name
         self.field = template.Variable(field_name)
+        self.use_bootstrap = use_bootstrap
 
     def render(self, context):
         bound_field = self.field.resolve(context)
         extra_attributes = dict(self.extra_attributes)
-        widget_template = get_template(get_widget_template_name(bound_field.field))
+        widget_template = get_template(get_widget_template_name(bound_field.field, self.use_bootstrap))
         value = bound_field.value()
         if isinstance(bound_field.field.widget, widgets.PasswordInput):
             value = ''
@@ -44,7 +49,7 @@ class MangleWidgetNode(template.Node):
 
 
 @register.tag()
-def mangle_widget(parser, token):
+def mangle_widget(parser, token, use_bootstrap=False):
     contents = token.split_contents()
     extra_attributes = {}
     if len(contents) > 2:
@@ -59,4 +64,9 @@ def mangle_widget(parser, token):
             extra_attributes[key] = value
     field_name = contents[1]
     # extra_attributes['class'] = 'form-control'
-    return MangleWidgetNode(field_name, extra_attributes)
+    return MangleWidgetNode(field_name, extra_attributes, use_bootstrap)
+
+
+@register.tag()
+def mangle_widget_bs3(parser, token):
+    return mangle_widget(parser, token, use_bootstrap=True)
